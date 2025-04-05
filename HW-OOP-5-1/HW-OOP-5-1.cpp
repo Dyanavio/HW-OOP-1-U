31ac3bc342e9a6b15a464cc84c6ac2da15e1536c
Dyanavio <dimant2018@gmail.com>
2025-04-05 12:18:10 +0300

    HW-5

---
+++ HW-OOP-5-1/HW-OOP-5-1.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include "color.hpp"
+
+using namespace std;
+
+static vector<int> vectorIncrement(vector<int> vtr)
+{
+    vector<int> v;
+    for (vector<int>::iterator it = vtr.begin(); it < vtr.end(); it++)
+    {
+        v.push_back(++(*it));
+    }
+    return v;
+}
+
+//По-ленивому
+static bool predicate(int i, int j) { return i > j; }
+static void sortByDescPredicate(vector<int>& vtr)
+{
+    sort(vtr.begin(), vtr.end(), predicate); 
+}
+
+//По-обычному
+static void sortByDesc(vector<int>& vtr)
+{
+    for (int i = 0; i < vtr.size(); i++)
+    {
+        for (int j = i; j < vtr.size(); j++)
+        {
+            if (vtr[i] < vtr[j])
+            {
+                int temp = vtr[i];
+                vtr[i] = vtr[j];
+                vtr[j] = temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    srand(time(NULL));
+    // Ну по идее я 30-й в списке, последняя цифра 0, поэтому я сделал первый и десятый пункты
+    vector<int> vtr(10);
+    for (vector<int>::iterator it = vtr.begin(); it < vtr.end(); it++)
+    {
+        *it += rand() % 25;
+    }
+    cout << dye::light_blue("Initial vector before increment function call:") << endl;
+    for (vector<int>::iterator it = vtr.begin(); it < vtr.end(); it++)
+    {
+        cout << *it << " ";
+    }
+    cout << endl;
+
+    vtr = vectorIncrement(vtr);
+    cout << dye::light_green("\nNew vector after increment function call:") << endl;
+    for (vector<int>::iterator it = vtr.begin(); it < vtr.end(); it++)
+    {
+        cout << *it << " ";
+    }
+    cout << endl;
+    //------------------------------------------------------------------------------
+    //sortByDesc(vtr);
+    sortByDescPredicate(vtr); // Оба варианта рабочие
+    cout << dye::light_red("\nSorted vector:") << endl;
+    for (vector<int>::iterator it = vtr.begin(); it < vtr.end(); it++)
+    {
+        cout << *it << " ";
+    }
+    cout << endl;
+}
+
---
+++ HW-OOP-5/HW-OOP-5.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include "color.hpp"
+
+using namespace std;
+
+static vector<int>::iterator find(vector<int>& vtr, int element) // Passing by the reference so we do not get ivalidated vector iterator error
+{
+    vector<int>::iterator it;
+    for (it = vtr.begin(); it < vtr.end(); it++)
+    {
+        if (*it == element) return it;
+    }
+    return it;
+}
+
+int main()
+{
+    vector<int> vtr;
+    int n;
+    cout << dye::yellow("--- Adding elements (0 to end) ---") << endl;
+    do
+    {
+        cout << "Enter value: ";
+        cin >> n;
+        vtr.push_back(n);
+    } while (n);
+
+    cout << dye::green("--- FOR CYCLE OUTPUT ---") << endl;
+    for (int i = 0; i < vtr.size(); i++)
+    {
+        cout << vtr[i] << " ";
+    }
+    cout << endl;
+
+    cout << dye::light_blue("--- ITERATOR OUTPUT ---") << endl;
+    vector<int>::iterator it;
+    for (it = vtr.begin(); it < vtr.end(); it++)
+    {
+        cout << *it << " ";
+    }
+    cout << endl;
+
+    cout << dye::red("--- DELETING THE LAST ELEMENT ---") << endl;
+    vtr.pop_back();
+    for (it = vtr.begin(); it < vtr.end(); it++)
+    {
+        cout << *it << " ";
+    }
+    cout << endl;
+
+    cout << dye::light_green("First element: ") << vtr.front() << endl;
+    cout << dye::light_green("Last element: ") << vtr.back() << endl;
+
+    cout << dye::purple("\n--- SORTING THE VECTOR ---") << endl;
+    sort(vtr.begin(), vtr.end());
+    for (it = vtr.begin(); it < vtr.end(); it++)
+    {
+        cout << *it << " ";
+    }
+
+    cout << dye::aqua("\nSize: ") << vtr.size() << endl;
+
+    cout << dye::light_yellow("--- FINDING AN ELEMENT ---") << endl;
+    int toFind;
+    cout << "Enter element: ";
+    cin >> toFind;
+    cout << "Element: " << *find(vtr, toFind) << endl;
+
+    cout << dye::red_on_white("\n--- CLEARING THE VECTOR ---") << endl;
+    vtr.clear();
+    cout << dye::red("Size: ") << vtr.size() << endl;
+}
